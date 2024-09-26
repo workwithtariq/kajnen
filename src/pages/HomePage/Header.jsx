3ce200d369f1e9f2412eb4f24d48bb866557ddf4
Tariq <workwithtariq@gmail.com>
2024-09-27 00:19:25 +0600

    redirect logo name to /

--- src/pages/HomePage/Header.jsx
+++ src/pages/HomePage/Header.jsx
@@ -1,10 +1,11 @@
 // components/Header.jsx
 import React from "react";
 import { AiOutlineMenu, AiOutlineClose } from "react-icons/ai";
+import { Link } from "react-router-dom";
 
 const Header = ({ isMenuOpen, toggleMenu }) => (
   <header className="bg-blue-500 text-white py-4 px-6 shadow-md flex justify-between items-center">
-    <div className="text-lg font-bold">কাজ_নেন</div>
+    <Link to="/" className="text-lg font-bold">কাজ-নেন</Link>
     <button onClick={toggleMenu} className="text-white text-2xl focus:outline-none">
       {isMenuOpen ? <AiOutlineClose /> : <AiOutlineMenu />}
     </button>
